92c883ffa945b8f87a210982369c5f1f48f4399a
= <=>
2026-06-15 15:33:56 +0200

    Projekt js

---
+++ javascript/Projekt.js
@@ -0,0 +1,30 @@
+class Projekt {
+
+    constructor(id, titel, kurzbeschreibung, projektlogo, startdatum)
+    {
+        this._id = id
+        this._titel = titel
+        
+        
+        this._projektlogo = projektlogo
+        this._startdatum = startdatum
+    }
+
+    set kurzbeschreibung( param1)
+    {
+        if(param1.length <= 255)
+        {
+            this._kurzbeschreibung = param1
+        }
+        else
+        {
+            console.log("kurzbeschreibung zu lang")
+        }
+        
+    }
+
+    get kurzbeschreibung()
+    {
+        return this._kurzbeschreibung
+    }
+}
